3e1e394b8175b09323c80e7b77f4c1521b318632
secret_online <me+github@secretonline.co>
2016-02-06 19:35:44 +1300

    Begin making structure
    Promises. Promises everywhere.
    
    There's a reason I like them. They make any sort of async chaining
    extremely easy to both write and read.

--- index.js
+++ index.js
@@ -2,3 +2,24 @@
 var Discord = require('discord.js');
 var IRC = require('irc');
 var config = require('./config.json');
+
+// Possibly change names, to reduce ambiguity with module and variable
+var discord = new Discord.Client();
+var irc = new IRC.Client(config.irc.server, config.irc.nick, {
+  autoConnect: false
+});
+
+function loginDiscord() {
+  // TODO: login to discord
+}
+
+function loginIrc() {
+  // TODO: login to irc
+}
+
+Promise.all([loginDiscord(), loginIrc()])
+  .catch(function(e) {
+    console.error('failed to login');
+    throw e;
+  })
+  .then();
